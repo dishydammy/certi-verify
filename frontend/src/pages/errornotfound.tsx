1a9e218bbe11ad9ed7fb8d141b34a2d4bc4f753b
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-08-01 16:07:46 +0100

    feat: add 404 page

---
+++ frontend/src/pages/errornotfound.tsx
@@ -0,0 +1,14 @@
+
+
+
+function ErrorNotFound() {
+  return (
+    <>
+      <div>
+       Error not found. The page you are looking for does not exist.
+      </div>
+    </>
+  )
+}
+
+export default ErrorNotFound;
